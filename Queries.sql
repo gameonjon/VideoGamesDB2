eca86e2a464ba0688f8b0e4880a4b30205633ca1
gameonjon <39804998+gameonjon@users.noreply.github.com>
2021-04-25 21:34:59 -0700

    more scratch paper nonsense

--- Queries.sql
+++ Queries.sql
@@ -4,6 +4,9 @@
 SELECT DISTINCT g_title
     FROM Games
 
+SELECT * FROM Games
+    WHERE g_gameID = 7
+
 -- for Reviews table
 SELECT g_title AS Game_Title, r_rating AS Rating, r_comment AS Review, r_resource AS Resource
     FROM Reviews, Games
@@ -176,7 +179,11 @@ SELECT DISTINCT g_title AS GameTitle, c_pubkey AS Publisher, d_name AS Developer
 
 --we also need a trigger to control the contracts update
 --this will go in VideoGame.py for DB setup
-CREATE TRIGGER insert_GCPD AFTER INSERT ON Games
+CREATE TRIGGER updateGamePlat AFTER UPDATE ON Games
+    WHEN 
+        NEW.
+
+declare tem
 
 
 CREATE TRIGGER insert_GCPD INSTEAD OF INSERT ON Games
@@ -224,6 +231,55 @@ CREATE VIEW GCPD(gameTitle, gameYear, genre, exKey, pubName, devName) AS
 DROP VIEW GCPD
 
 INSERT INTO GCPD
+
+
+
+
+-- PATCH request for direct game values (and Platform) only
+UPDATE Games g, Platform p
+    SET g.g_exkey = p.pf_exkey
+    WHERE g_gameID = 18 AND
+        pf_system = 'PC, Playstation'
+
+UPDATE Games, Platform SET
+    g_title = COALESCE(?, g_title),
+    g_year = COALESCE(?, g_year),
+    g_genre = COALESCE(?, g_genre),
+    g_exkey = COALESCE(?, )
+    WHERE g_gameID = ? AND
+        pf_system = 
+        
+UPDATE Games SET
+    g_title = COALESCE(?, g_title),
+    g_year = COALESCE(?, g_year),
+    g_genre = COALESCE(?, g_genre)
+    WHERE g_gameID = ?
+
+UPDATE Games SET
+    g_title = "Dying Retribution",
+    g_genre = "Action-Horror"
+    WHERE g_gameID = 7
+
+
+--Pretty sure that COALESCE is an SQL Server Function Only, wont work if you run it here
+UPDATE Games
+    SET g_exkey = COALESCE(SELECT pf_exkey FROM Platform WHERE pf_system = ?, g_exkey)
+    WHERE g_gameID = ?;
+
+
+UPDATE Games
+    SET g_exkey = (SELECT pf_exkey FROM Platform WHERE pf_system = 'PC,Xbox,Nintendo')
+    WHERE g_gameID = 7;
+    
+
+UPDATE Games
+    SET g_exkey = 
+    WHERE g_gameID = 18 AND
+        g_exkey = (SELECT pf_exkey
+                        FROM Platform
+                        WHERE pf_system = 'PC,Playstation')
+        
+
 
 
 
